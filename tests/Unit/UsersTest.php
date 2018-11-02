a1ad5771db98eeb1e065671b93fb3f7613069af9
dhaval <dk375@njit.edu>
2018-11-02 15:23:06 -0400

    Test: unit test to insert a user into the users table

---
+++ tests/Unit/UsersTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertUser()
+    {
+//        $this->assertTrue(true);
+        $user = new User;
+        $user->name = 'Dhaval Khatri';
+        $user->email = 'dhaval@njit.edu';
+        $user->password = '1234';
+        $user->save();
+        $this->assertTrue($user->forceDelete());
+    }
+}
